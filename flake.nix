8901e62e6a28da8092c9fa79bc44df0a089f793f
Vincent Thomas <vincent@v-thomas.com>
2026-07-28 01:05:23 +0200

    nix nice  thing

--- flake.nix
+++ flake.nix
@@ -1,7 +1,20 @@
 {
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
+
+    bun2nix.url = "github:nix-community/bun2nix";
+    bun2nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -9,19 +22,44 @@
       self,
       nixpkgs,
       flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.bun
-            pkgs.gnumake
-          ];
-        };
-      }
-    );
+      ...
+    }@inputs:
+    let
+      eachSystem = inputs.nixpkgs.lib.genAttrs (import inputs.systems);
+      # Access the package set for a given system
+      pkgsFor = eachSystem (
+        system:
+        import inputs.nixpkgs {
+          inherit system;
+          # Use the bun2nix overlay, which puts `bun2nix` in pkgs
+          # You can, of course, still access
+          # inputs.bun2nix.packages.${system}.default instead
+          # and use that to build your package instead
+          overlays = [ inputs.bun2nix.overlays.default ];
+        }
+      );
+    in
+    {
+      packages = eachSystem (system: {
+        # Produce a package for this template with bun2nix in
+        # the overlay
+        coder = pkgsFor.${system}.callPackage ./nix/coder.nix { };
+      });
+
+    };
+  # // flake-utils.lib.eachDefaultSystem (
+  #   system:
+  #   let
+  #     pkgs = nixpkgs.legacyPackages.${system};
+  #   in
+  #   {
+  #
+  #     # devShells.default = pkgs.mkShell {
+  #     #   buildInputs = [
+  #     #     pkgs.bun
+  #     #     pkgs.gnumake
+  #     #   ];
+  #     # };
+  #   }
+  # );
 }
---
+++ nix/coder.nix
@@ -0,0 +1,44 @@
+{
+  bun,
+  bun2nix,
+  writeShellApplication,
+  ...
+}:
+# bun2nix.mkDerivation {
+#   pname = "workspace-test-app";
+#   version = "1.0.0";
+#
+#   src = ./.;
+#
+#   bunDeps = bun2nix.fetchBunDeps {
+#     bunNix = ./bun.nix;
+#   };
+#
+#   module = "packages/agent/src/index.ts";
+# }
+#
+let
+  application = bun2nix.writeBunApplication {
+    pname = "coder";
+    version = "1.0.0";
+
+    src = ./..;
+
+    startScript = ''
+      bun run ./packages/agent/src/index.ts
+    '';
+
+    buildPhase = ":";
+
+    bunDeps = bun2nix.fetchBunDeps {
+      bunNix = ../bun.nix;
+    };
+  };
+in
+writeShellApplication {
+  name = "coder";
+  runtimeInputs = [ bun ];
+  text = ''
+    exec bun run ${application}/share/coder/packages/agent/src/index.ts "$@"
+  '';
+}
